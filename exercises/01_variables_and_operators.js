f4c10165ccf43b51045765f8ef4a0a8f9b4aa7d1
fedeargfyron <fedemgs15@gmail.com>
2022-05-21 16:08:59 -0300

    Se resuelve ejercicio 1

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,12 @@
+// Variables
+var string1 = 'Hola';
+var string2 = 'soy Fede';
+
+// Ejercicio 1
+var stringConcat = string1.concat(' ', string2);
+console.log(stringConcat);
+
+// Ejercicio 2
+var stringLength = string1.length + string2.length;
+console.log(stringLength);
+
